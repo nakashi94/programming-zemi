f5f6687c87195c476eec5576820c1a5175565a70
nakashi94 <n0904yBUNJOU@gmail.com>
2021-09-10 18:21:16 +0000

    basic signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
   get 'home' => "static_pages#home"
   get 'help' => "static_pages#help"
   get 'about' => "static_pages#about"
   get 'contact' => "static_pages#contact"
   get  'signup' => "users#new"
-<<<<<<< HEAD
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
-=======
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  get 'hello-world' => "foo#hello"
-  
->>>>>>> effb6510fbfb9d3f423cfc18c740cdffdf9a4907
+  resources :users
+
 end
