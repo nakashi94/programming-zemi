f9c1599ad29398e451b3f1ef99161aab7431f94b
nakashi94 <n0904yBUNJOU@gmail.com>
2021-09-12 02:53:50 +0000

    tweet

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,11 @@ Rails.application.routes.draw do
   get 'help' => "static_pages#help"
   get 'about' => "static_pages#about"
   get 'contact' => "static_pages#contact"
-  get  'signup' => "users#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   resources :users
-
+  resources :tweets, only: [:create, :edit, :update, :destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_10_183705) do
+ActiveRecord::Schema.define(version: 2021_09_11_111450) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
